3a1b02c0d0f431cc043101b5f5f016c3bd5436a1
christian482 <46334785+christian482@users.noreply.github.com>
2019-02-08 17:28:56 -0600

    fsdfs

--- index.php
+++ index.php
@@ -77,7 +77,7 @@ $botman = BotManFactory::create($config, new DoctrineCache($doctrineCacheDriver)
 $botman->hears('^(?!.*\basesor|ASESOR|Asesor\b).*$', function (BotMan $bot) {
   $nombre = $bot->getUserWithFields(["first_name"]);
   $nombre = $nombre->getFirstName();
-  $bot -> reply("Bienvenido $nombre. te comento que nos  especializamos en la asesoria para Pensionados y Jubilados del IMSS, de igual forma contamos con asesoria para activos del IMSS. ");
+  $bot -> reply("Bienvenido $nombre te comento que nos  especializamos en la asesoria para Pensionados y Jubilados del IMSS, de igual forma contamos con asesoria para activos del IMSS. ");
   $bot -> startConversation(new MenuConversation($nombre));
 });
 
