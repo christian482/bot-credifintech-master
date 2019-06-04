2ff0ed9ae269fa24f78655aeb7b2af3295058cc5
christian482 <46334785+christian482@users.noreply.github.com>
2019-06-03 23:42:34 -0500

    Update curlwrap_v2.php

--- src/curlwrap_v2.php
+++ src/curlwrap_v2.php
@@ -1,5 +1,5 @@
 <?php
-   /** Get all leads with a limit of 500 results */
+   /*
    $limit = 500;
    $offset = 0;
 
@@ -33,5 +33,54 @@ function curl_wrap($params) {
    curl_close($ch);
 
    echo $result;
+}
+*/
+
+function curl_wrap($entity, $data, $method, $content_type) {
+    if ($content_type == NULL) {
+        $content_type = "application/json";
+    }
+
+    $agile_url = "https://" . "credifintech" . ".agilecrm.com/dev/api/" . $entity;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+    curl_setopt($ch, CURLOPT_UNRESTRICTED_AUTH, true);
+    switch ($method) {
+        case "POST":
+            $url = $agile_url;
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+            break;
+        case "GET":
+            $url = $agile_url;
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+            break;
+        case "PUT":
+            $url = $agile_url;
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+            break;
+        case "DELETE":
+            $url = $agile_url;
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+            break;
+        default:
+            break;
+    }
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        "Content-type : $content_type;", 'Accept : application/json'
+    ));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_USERPWD,  "oberumen@me.com" . ':' . "5r7712igopdds1tvv2f2mma4or");
+    curl_setopt($ch, CURLOPT_TIMEOUT, 120);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $output;
 }
 ?>
